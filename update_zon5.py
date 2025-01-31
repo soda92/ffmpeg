ee7f2d64ac96d69ddb394e570e4148b93a454bbe
sodacris <wjxa20152015@gmail.com>
2025-01-31 17:13:32 +0800

    add cMacro fix

---
+++ update_zon5.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+
+def replace(s):
+    s0, s1, s2 = s.split()
+    c = Path(s0).read_text(encoding="utf8")
+    c = c.replace(s1, s2)
+    Path(s0).write_text(c, encoding="utf8")
+
+
+replace(
+    "build.zig.zon unzip/github.com/allyourcodebase/libressl/archive/refs/tags/libressl-3.9.2+1 unzip/github.com/allyourcodebase/libressl/archive/refs/tags/libressl-3.9.2-1"
+)
+
+replace(
+    r"unzip\github.com\allyourcodebase\mbedtls\build.zig.zon unzip/github.com/Mbed-TLS/mbedtls/archive/refs/tags/mbedtls-3.6.1 unzip/github.com/Mbed-TLS/mbedtls/archive/refs/mbedtls-mbedtls-3.6.1"
+)
+
+replace(
+    r"unzip\github.com\allyourcodebase\zlib\archive\refs\tags\zlib-1.3.1-3\build.zig.zon unzip/github.com/madler/zlib/archive/refs/tags/zlib-1.3 unzip/github.com/madler/zlib/archive/refs/zlib-1.3 "
+)
---
+++ update_zon6.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import os
+
+import contextlib
+
+
+@contextlib.contextmanager
+def CD(d):
+    old = os.getcwd()
+    os.chdir(d)
+    yield
+
+    os.chdir(old)
+
+
+if __name__ == "__main__":
+    import glob
+
+    files = glob.glob("**/*.zig", recursive=True)
+    for file in files:
+        lines = []
+        file = Path(file)
+        content = file.read_text(encoding="utf8")
+        for line in content.split("\n"):
+            if (
+                "defineCMacro" in line
+                and "LibreSslCommon, name: []" not in line
+                and "defineCMacroForLibs" not in line
+            ):
+                print(line)
+                line = line.replace("defineCMacro", "root_module.addCMacro")
+                line = line.replace("null", '""')
+                line = line.replace("libressl_common.root_module", "libressl_common.libcrypto.root_module")
+                lines.append(line)
+            else:
+                lines.append(line)
+        file.write_text("\n".join(lines))
