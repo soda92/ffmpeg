1acf2433e47f55019162a067c659356f5c9abfc4
sodacris <wjxa20152015@gmail.com>
2025-01-31 16:09:27 +0800

    merge and rename

--- update_zon.py
+++ update_zon.py
@@ -1,6 +1,38 @@
 from pathlib import Path
 import re
 import subprocess
+import glob
+
+CURRENT = Path(__file__).resolve().parent
+content = CURRENT.joinpath("build.zig.zon").read_text(encoding="utf8")
+
+urls = re.findall(r'.url = "(https.*\.tar.gz)"', content)
+
+print(urls)
+
+for i in urls:
+    p = i.replace("https://", "tmp/")
+    p = CURRENT.joinpath(p)
+    p.parent.mkdir(parents=True, exist_ok=True)
+    # print(p)
+
+    subprocess.run(["wget", i, "-O", str(p)], check=True)
+
+CURRENT = Path(__file__).resolve().parent
+
+gz_files = glob.glob("**/*.tar.gz", recursive=True, root_dir=CURRENT.joinpath("tmp"))
+
+for file in gz_files:
+    target_dir = CURRENT.joinpath("unzip").joinpath(file)
+    orig_file = CURRENT.joinpath("tmp").joinpath(file)
+
+    print(orig_file)
+    print(target_dir)
+
+    target_dir.parent.mkdir(exist_ok=True, parents=True)
+
+    subprocess.run(["tar", "xf", orig_file, "-C", target_dir.parent])
+
 
 CURRENT = Path(__file__).resolve().parent
 content = CURRENT.joinpath("build.zig.zon").read_text(encoding="utf8")
