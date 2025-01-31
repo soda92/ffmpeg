d0682143d78f944c225d3373da0bf9b4401841b0
sodacris <wjxa20152015@gmail.com>
2025-01-31 15:22:47 +0800

    1st replace

--- update_zon.py
+++ update_zon.py
@@ -5,14 +5,13 @@ import subprocess
 CURRENT = Path(__file__).resolve().parent
 content = CURRENT.joinpath("build.zig.zon").read_text(encoding="utf8")
 
-urls = re.findall(r'.url = "(https.*\.tar.gz)"', content)
+urls = re.findall(r'(.url = "https.*\.tar.gz")', content)
 
 print(urls)
 
 for i in urls:
-    p = i.replace("https://", "tmp/")
-    p = CURRENT.joinpath(p)
-    p.parent.mkdir(parents=True, exist_ok=True)
-    # print(p)
+    p = i.replace("https://", "unzip/").replace(".url", ".path").replace(".tar.gz", "")
+    content = content.replace(i, p)
 
-    subprocess.run(["wget", i, "-O", str(p)], check=True)
+content = "\n".join(filter(lambda x: ".hash" not in x, content.split("\n")))
+CURRENT.joinpath("build.zig.zon").write_text(content, encoding="utf8")
