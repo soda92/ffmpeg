8fd812ea95c03383781df953b44fe4a4bd231cfc
sodacris <wjxa20152015@gmail.com>
2025-01-31 18:47:55 +0800

    add windows patch files

---
+++ update_zon8.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.copy(r"z_patched_files\compiler.h", r"unzip\github.com\allyourcodebase\nasm\archive\refs\tags\nasm-2.16.1-3\include\compiler.h")
+shutil.copy(r"z_patched_files\file.h", r"unzip\github.com\allyourcodebase\nasm\archive\refs\tags\nasm-2.16.1-3\nasmlib\file.h")
