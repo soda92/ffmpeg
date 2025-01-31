34030218c57916f09d8d5d67e916ab8c71bbc800
sodacris <wjxa20152015@gmail.com>
2025-01-31 17:47:43 +0800

    fix relative path

---
+++ update_zon7.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import os
+import re
+
+import contextlib
+
+
+@contextlib.contextmanager
+def CD(d):
+    old = os.getcwd()
+    os.chdir(d)
+    yield
+
+    os.chdir(old)
+
+
+if __name__ == "__main__":
+    import glob
+
+    files = glob.glob("**/*.zon", recursive=True)
+    for file in files:
+        lines = []
+        file = Path(file)
+        content = file.read_text(encoding="utf8")
+        for line in content.split("\n"):
+            if ".path = " in line:
+                content = re.findall(r'"(.*)"', line)[0]
+                print(content)
+
+                content2 = content[content.index("unzip/github.com"):]
+                levels = str(file).count("\\")
+                c2 = "../" * levels + content2
+                line = line.replace(content, c2)
+
+            lines.append(line)
+        file.write_text("\n".join(lines), encoding="utf8")
